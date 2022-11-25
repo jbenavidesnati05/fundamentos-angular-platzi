3dd8f5c132e2549535a04969f25e32e5d3009095
Jorge Benavides Nati <jbenavides@sistecredito.com>
2022-11-25 11:13:44 -0500

    subiendo event

--- src/app/components/event-binding/event-binding.component.ts
+++ src/app/components/event-binding/event-binding.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-event-binding',
   templateUrl: './event-binding.component.html',
   styleUrls: ['./event-binding.component.css']
 })
-export class EventBindingComponent implements OnInit {
+export class EventBindingComponent{
 
-  constructor() { }
 
-  ngOnInit(): void {
+  btnDisabled = true;
+  activo = false;
+  desactivado = true;
+  edadActual = 19;
+
+  activarDesactivar(){
+    this.btnDisabled = !this.btnDisabled
+    this.activo = !this.activo
+    this.desactivado = !this.desactivado
+
   }
 
+  sumarEdad(){
+    this.edadActual = this.edadActual +1
+  }
+
+  restarEdad(){
+    this.edadActual = this.edadActual -1
+  }
 }
